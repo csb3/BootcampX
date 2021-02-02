282441adc55dd5f6763aad81704e385028065264
Cass Bredeson <c.s.bredeson@gmail.com>
2021-02-02 21:27:03 +0000

    added assistance request/teacher queries

---
+++ 4_queries/9_cohort_average_assistance_duration.sql
@@ -0,0 +1,6 @@
+SELECT AVG(cohort_totals.total_duration) as average_total_duration FROM (SELECT cohorts.name AS cohort, SUM(completed_at - started_at) AS total_duration
+FROM assistance_requests
+JOIN students ON student_id = students.id
+JOIN cohorts ON students.cohort_id = cohorts.id
+GROUP BY cohorts.name
+ORDER BY total_duration) AS cohort_totals;
